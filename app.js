5a2a5c209bc03c9d2b397e4f04a39b3bd35a0ca2
Daniel Weber <1678401@park.edu>
2022-01-16 01:44:09 -0600

    added routes, controllers, schema, model, logic for getting all tasks, single tasks, and adding tasks

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
 const express = require("express");
 const app = express();
+const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
+require("dotenv").config();
+
+// middleware
+app.use(express.json());
+
+// routes
+app.get("/hello", (req, res) => {
+  res.status(200).send("Hello");
+});
+
+app.use("/api/v1/tasks", tasks);
 
 const port = 3000;
 
-app.listen(port, () => {
-  console.log(`The server is running on port ${port}...`);
-});
+const start = async () => {
+  // make sure database is connected before starting server
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => {
+      console.log(`The server is running on port ${port}...`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
---
+++ controllers/tasks.js
@@ -0,0 +1,43 @@
+const Task = require("../models/task");
+
+const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.status(200).json({ tasks });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+  res.send("all tasks");
+};
+
+const createTask = async (req, res) => {
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+};
+
+const getTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = Task.findOne({ _id: taskID });
+    if (!task) {
+      return res.status(404).json({ msg: `No task with ID: ${taskID}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+};
+
+const updateTask = (req, res) => {
+  res.send("update task");
+};
+
+const deleteTask = (req, res) => {
+  res.send("delete task");
+};
+
+module.exports = { getAllTasks, getTask, createTask, updateTask, deleteTask };
