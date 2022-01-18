d9fc588e225c87ed4447af0b1a1ef0b96a1df642
Daniel Weber <1678401@park.edu>
2022-01-17 23:05:09 -0600

    refactored controllers to use asyc wrapper function, created custom error and handler

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect");
+const notFound = require("./middleware/not-found");
+const errorHandler = require("./middleware/error-handler");
 require("dotenv").config();
 
 // middleware
@@ -10,7 +12,9 @@ app.use(express.json());
 
 app.use("/api/v1/tasks", tasks);
 
-const port = 3000;
+app.use(notFound);
+app.use(errorHandler);
+const port = process.env.PORT || 3000;
 
 const start = async () => {
   // make sure database is connected before starting server
